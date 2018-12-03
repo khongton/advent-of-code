7b2f5ac85d00c702ea58bbc3df9db09f151919ed
Kevin Hongtongsak <kevin.h@yubico.com>
2018-12-02 18:00:26 -0800

    day 2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,5 +1,7 @@
 from collections import Counter
 from functools import reduce
+from difflib import SequenceMatcher, ndiff
+from operator import itemgetter
 
 with open('input.txt', 'r') as inputFile:
     lines = inputFile.readlines()
@@ -11,3 +13,18 @@ with open('input.txt', 'r') as inputFile:
             if num in checksum:
                 checksum[num] += 1
     print(reduce((lambda x, y: x * y), list(checksum.values())))
+
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+    workingData = []
+    for idx, comparer in enumerate(lines):
+        for comparee in lines[idx:]:
+            if comparer != comparee:
+               workingData.append((comparer, comparee, SequenceMatcher(None, comparer, comparee).ratio()))
+    matchingIDs = max(workingData, key=itemgetter(2))
+
+    resultStr = ''
+    for x, y in zip(matchingIDs[0], matchingIDs[1]):
+        if x == y:
+            resultStr += x
+    print(resultStr)
