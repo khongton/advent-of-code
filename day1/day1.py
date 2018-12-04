d6dfef73ed59059b7db7c46335ffc46a2088e8aa
Kevin Hongtongsak <kevin.h@yubico.com>
2018-12-04 08:27:15 -0800

    day1 cleanup

--- day1/day1.py
+++ day1/day1.py
@@ -4,17 +4,19 @@ f = open('input.txt')
 lines = f.readlines()
 f.close()
 
-sum = 0
-for line in lines:
-    sum += int(line)
-print(sum)
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+    sum = 0
+    for line in lines:
+        sum += int(line)
+    print(sum)
 
-sums = {}
-curSum = 0
-for line in itertools.cycle(lines):
-    curSum += int(line);
-    if curSum in sums:
-        print(curSum)
-        break
-    else:
-        sums[curSum] = 1
+    sums = {}
+    curSum = 0
+    for line in itertools.cycle(lines):
+        curSum += int(line)w
+        if curSum in sums:
+            print(curSum)
+            break
+        else:
+            sums[curSum] = 1
